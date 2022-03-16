0ba8c83d4af567751cdee7c69fa22d1a7f8c25c8
Đỗ Vương Quốc Thịnh <37610310+dvqthinh25111999@users.noreply.github.com>
2022-03-16 21:47:03 +0700

    update LoginHeader.h

--- LoginHeader.h
+++ LoginHeader.h
@@ -1,6 +1,32 @@
-#ifndef LOGINHEADER_H_INCLUDED
-#define LOGINHEADER_H_INCLUDED
-
-void Login();
-
-#endif // LOGINHEADER_H_INCLUDED
+#ifndef _LOGIN_H_
+#define _LOGIN_H_
+#include<iostream>
+#include<fstream>
+#include<string>
+#include"Utils.h"
+#include"Const.h"
+#include"ViewInfo.h"
+using namespace std;
+class Login {
+private:
+	Login();
+	char username[50];
+	char *password;
+	static const char defaultPass = '1';
+	int foundUsername(char filename[]);
+	int passWord(char filename[]);
+	bool match(char inputted[], char sN[]);
+	
+	int changePasswordInit(char filename[]);
+	void deLogin();
+	
+public:
+	int stt;//so thu tu cua hoc sinh trong file
+	int type;//student/lecturer/staff
+	Login();
+	~Login();
+	void login();
+	void logout();
+	void changePassword(char filename[]);
+};
+#endif
