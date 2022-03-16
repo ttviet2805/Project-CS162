add588d0e53e27d020947c6ee87c9a21de628322
Đỗ Vương Quốc Thịnh <37610310+dvqthinh25111999@users.noreply.github.com>
2022-03-16 21:51:01 +0700

    Update LoginFunction.cpp

--- LoginFunction.cpp
+++ LoginFunction.cpp
@@ -5,3 +5,16 @@ void Login::logout() {
 
 	Login();
 }
+
+void Login::changePassword(char filename[]) {
+	cout << "Change password" << endl;
+	cout << "Please, retype your current password: ";
+	char oldP[200];
+	do {
+		cin.get(oldP, 200, '\n');
+		cin.ignore(200, '\n');
+		if (match(password, oldP)) break;
+		else cout << "Wrong password, please retype: ";
+	} while (1);
+	changePasswordInit(filename);
+}
