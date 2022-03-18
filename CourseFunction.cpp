179adc64fa0985c654603cac6482bb78d5305cde
MiGyi <94895248+MiGyi@users.noreply.github.com>
2022-03-18 21:47:26 +0700

    Add files via upload
    Thêm delete, save, load
    delete và load nằm ngoài class course

--- CourseFunction.cpp
+++ CourseFunction.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <fstream>
+#include <unistd.h>
 
 #include "DateHeader.h"
 #include "CourseHeader.h"
 
 using namespace std;
+
 
 void Course::CourseInfo()
 {
@@ -15,12 +17,13 @@ void Course::CourseInfo()
     cout << Lecturer << LecturerName << '\n';
     cout << Start; StartDate.OutputDate();
     cout << End; EndDate.OutputDate();
-    cout << Ses1 << 'S' << FirstS << ' ';
-    cout << Ses2 << 'S' << SecondS;
+    cout << Ses1 << ": " << FirstS << '\n';
+    cout << Ses2 << ": " << SecondS << '\n';
 }
 
 void Course::CourseInfoWithNumber()
 {
+    system("cls");
     char Name[] = "Name: ", ID[] = "ID: ", Lecturer[] = "Lecturer: ", Start[] = "Start day: ", End[] = "End day: ",
                     Ses1[] = "Session 1: ", Ses2[] = "Session 2: ";
     cout << "1. " << Name << CourseName << '\n';
@@ -28,32 +31,28 @@ void Course::CourseInfoWithNumber()
     cout << "3. " << Lecturer << LecturerName << '\n';
     cout << "4. " << Start; StartDate.OutputDate();
     cout << "5. " << End; EndDate.OutputDate();
-    cout << "6. " << Ses1 << 'S' << FirstS << ' ';
-    cout << "7. " << Ses2 << 'S' << SecondS;
+    cout << "6. " << Ses1 << ": " << FirstS << '\n';
+    cout << "7. " << Ses2 << ": " << SecondS << '\n';
 }
 
-void Course::LoadLastCoursesData(char Filename[])
+void Course::SaveCoursesData(char Filename[])
 {
-    ifstream fi(Filename);
-    int init = 1;
-    Course *Dummy = this;
-    while (!fi.eof())
+    ofstream fo(Filename);
+    Course *Cur = this;
+    while (Cur)
     {
-        if (!init) Dummy->Next = new Course;
-            else init--;
-        fi.getline(Dummy->CourseName, 50);
-        fi.getline(Dummy->CourseID, 50);
-        fi.getline(Dummy->LecturerName, 50);
-        int d, m, y;
-        fi >> d >> m >> y;
-        Dummy->StartDate = Date({d, m, y});
-        fi >> d >> m >> y;
-        Dummy->EndDate = Date({d, m, y});
-        fi >> Dummy->FirstS >> Dummy->SecondS;
+        fo << Cur->CourseName << '\n';
+        fo << Cur->CourseID << '\n';
+        fo << Cur->LecturerName << '\n';
+        fo << Cur->StartDate.Day << " " << Cur->StartDate.Month << Cur->StartDate.Year << '\n';
+        fo << Cur->EndDate.Day << " " << Cur->EndDate.Month << Cur->EndDate.Year << '\n';
+        fo << Cur->FirstS << '\n' << Cur->SecondS << '\n';
     }
+
+    fo.close();
 }
 
-void Course::Update()
+void Course::Update(char Filename[])
 {
     while (1)
     {
@@ -63,10 +62,10 @@ void Course::Update()
         for (Course *cur = this; cur; cur = cur->Next, i++)
             cout << i << ". " << cur->CourseName << '\n';
         int num;
-        cout << "Return = 0";
+        cout << "Return and save = 0\n";
         cout << "Please input a number: ";
         cin >> num;
-        if (num == 0) return;
+        if (num == 0) break;
         if (num >= i) continue;
         Course *Cur = this;
         for (num--; num > 0; num--) Cur = Cur->Next;
@@ -77,28 +76,36 @@ void Course::Update()
             cout << Cur->CourseName << '\n';
             cout << "Choose element: \n";
             Cur->CourseInfoWithNumber();
-            int num;
-            cout << "Return = 0\n";
+            int part;
+            cout << "Return and save = 0\n";
             cout << "Please input a number: ";
-            cin >> num;
-            if (num == 0) break;
-            if (num > 7) continue;
+            cin >> part;
+            if (part == 0) break;
+            if (part > 7)
+            {
+                cout << "Please input again!";
+                sleep(2);
+                continue;
+            }
             system("cls");
             cout << "Input new data: ";
-            switch (num)
+            switch (part)
             {
                 case 1:
                 {
+                    cin.ignore();
                     cin.getline(Cur->CourseName, 50);
                     break;
                 }
                 case 2:
                 {
+                    cin.ignore();
                     cin.getline(Cur->CourseID, 50);
                     break;
                 }
                 case 3:
                 {
+                    cin.ignore();
                     cin.getline(Cur->LecturerName, 50);
                     break;
                 }
@@ -114,23 +121,116 @@ void Course::Update()
                 }
                 case 6:
                 {
-                    char c;
-                    cin >> c;
-                    cin >> Cur->FirstS;
+                    cin.ignore();
+                    cin.getline(Cur->FirstS, 50);
                     break;
                 }
                 default:
                 {
-                    char c;
-                    cin >> c;
-                    cin >> Cur->SecondS;
+                    cin.ignore();
+                    cin.getline(Cur->SecondS, 50);
                 }
             }
         }
     }
+
+    SaveCoursesData(Filename);
 }
 
-void Course::Delete()
+void LoadLastCoursesData(Course *&Head, char Filename[])
 {
+    ifstream fi(Filename);
+    Head = new Course;
+    Course *Dummy = Head;
+    while (!fi.eof())
+    {
+        Dummy->Next = new Course, Dummy = Dummy->Next;
+        fi.getline(Dummy->CourseName, 50);
+        fi.getline(Dummy->CourseID, 50);
+        fi.getline(Dummy->LecturerName, 50);
+        int d, m, y;
+        fi >> d >> m >> y;
+        Dummy->StartDate = Date({d, m, y});
+        fi >> d >> m >> y;
+        Dummy->EndDate = Date({d, m, y});
+        fi.ignore();
+        fi.getline(Dummy->FirstS, 50);
+        fi.getline(Dummy->SecondS, 50);
+    }
+    Course *pD = Head;
+    Head = Head->Next;
+    delete(pD);
+    fi.close();
+}
 
+void Delete(Course *&Head, char Filename[])
+{
+    while (1)
+    {
+        system("cls");
+        if (Head == nullptr)
+        {
+            cout << "No more course!";
+            sleep(2);
+            break;
+        }
+        cout << "Choose course: \n";
+        int i = 1;
+        for (Course *cur = Head; cur; cur = cur->Next, i++)
+            cout << i << ". " << cur->CourseName << '\n';
+        int num;
+        cout << "Return and save = 0\n";
+        cout << "Please input a number: ";
+        cin >> num;
+        if (num == 0) break;
+        if (num >= i)
+        {
+            cout << "Please input again!";
+            sleep(2);
+            continue;
+        }
+        bool Recheck = 0;
+        cout << "Are you sure? (Y/N): ";
+        char c;
+        cin >> c;
+        Recheck = (c == 'Y');
+        if (!Recheck) continue;
+        Course *Cur = Head;
+        for (num--, num--; num > 0; num--) Cur = Cur->Next;
+        if (num == -1)
+        {
+            Course *pD = Head;
+            Head = Head->Next;
+            delete(pD);
+        }
+        else
+        {
+            Course *pD = Cur->Next;
+            Cur->Next = pD->Next;
+            delete(pD);
+        }
+    }
+
+    Head->SaveCoursesData(Filename);
+}
+
+void Course::AllCoursesInfo()
+{
+    system("cls");
+    Course *Cur = this;
+    while (Cur)
+    {
+        Cur->CourseInfo();
+        Cur = Cur->Next;
+    }
+}
+
+void DeleteCourse(Course *&Head)
+{
+    while (Head)
+    {
+        Course *pD = Head;
+        Head = Head->Next;
+        delete(pD);
+    }
 }
--- CourseHeader.h
+++ CourseHeader.h
@@ -1,11 +1,13 @@
 #ifndef COURSEHEADER_H_INCLUDED
 #define COURSEHEADER_H_INCLUDED
 
+#include "DateHeader.h"
+
 class Course {
 	public:
         char CourseName[50], CourseID[50], LecturerName[50];
         Date StartDate, EndDate;
-        int FirstS, SecondS; //(S1 (07:30), S2 (09:30), S3(13:30) and S4 (15:30))
+        char FirstS[50], SecondS[50]; //(S1 (07:30), S2 (09:30), S3(13:30) and S4 (15:30))
             // 2 sessions in a week
             // (nhiều nhất 5 course, không thể enroll course trùng lịch với các courses đã đăng kí)
         //ScoreBoard Scoreboard;
@@ -13,10 +15,14 @@ class Course {
 
         void CourseInfo();
         void CourseInfoWithNumber();
-        void LoadLastCoursesData(char Filename[]);
-        void Update();
-        void Delete();
+        void Update(char Filename[]);
+        void SaveCoursesData(char Filename[]);
+        void AllCoursesInfo();
 };
 
+void LoadLastCoursesData(Course *&Head, char Filename[]);
+void DeleteAllCourse(Course *Head);
+void Delete(Course *&Head, char Filename[]);
+void DeleteCourse(Course *&Head);
 
 #endif // COURSEHEADER_H_INCLUDED
