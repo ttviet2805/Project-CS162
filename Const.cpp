4b5d00adddfa324be2b92fef9e6b938db5fa7fda
Đỗ Vương Quốc Thịnh <37610310+dvqthinh25111999@users.noreply.github.com>
2022-03-19 10:53:06 +0700

    Create Const.cpp for output csv file

---
+++ Const.cpp
@@ -0,0 +1,7 @@
+#include"Const.h"
+char Const::usernameStudent[] = "username_student.csv";
+char Const::usernameSL[] = "username_staff_lecturer.csv";
+char Const::passwordStudent[] = "password_student.csv";
+char Const::passwordSL[] = "password_staff_lecturer.csv";
+char Const::infoStudent[] = "info_student.csv";
+char Const::infoSL[] = "info_staff_lecturer.csv";
