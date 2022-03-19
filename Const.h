1de3452caf11f07c721a6b5c3395fa511ab99207
Đỗ Vương Quốc Thịnh <37610310+dvqthinh25111999@users.noreply.github.com>
2022-03-19 10:44:28 +0700

    Update Const.h

---
+++ Const.h
@@ -0,0 +1,14 @@
+#ifndef _CONST_H_
+#define _CONST_H_
+class Const {
+public:
+	static char usernameStudent[];
+	static char usernameSL[];
+	static char passwordStudent[];
+	static char passwordSL[];
+	static char typeSL[];
+	static char infoStudent[];
+	static char infoSL[];
+
+};
+#endif
