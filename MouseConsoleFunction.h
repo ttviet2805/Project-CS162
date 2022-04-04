c194e8c512c4e391ee4e85c31d87b883e46bc573
Trần Việt <62978375+ttviet2805@users.noreply.github.com>
2022-04-04 16:28:31 +0700

    Delete MouseConsoleFunction.h

--- MouseConsoleFunction.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <windows.h>
-#include <tchar.h>
-#include <stdio.h>
-
-bool MouseEventProc(MOUSE_EVENT_RECORD);
-
-int checkMouseInButton(int X, int Y, int sizeX, int sizeY);
